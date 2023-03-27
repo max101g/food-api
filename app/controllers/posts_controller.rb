e20aa4db4448e34fad18b7a7d6fcf4c24088c866
max101g <maxgithinji101@gmail.com>
2023-03-27 11:48:10 +0300

    commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,35 @@
 class PostsController < ApplicationController
 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActionController::UnpermittedParameters, with: :handle_errors
+
     def index
         posts = Post.all 
         render json: posts
+    end
+
+    def create
+        user = User.create!(post_params)
+        render json: user
+    end
+
+    private
+
+    def post_params
+        params.permit(:location, :image)
+    end
+
+    def render_not_found_response
+        render json: { error: "Power not found" }, status: :not_found 
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def handle_errors
+        render json: { "Unpermitted Parameters": params.to_unsafe_h.except(:controller, :action, :name, :description)}, status: :unprocessable_entity 
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,44 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActionController::UnpermittedParameters, with: :handle_errors
 
     def index
         users = User.all
         render json: users
+    end
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user
+    end
+
+    def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user
+    end
+
+    private
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def user_params
+        params.permit(:name, :username, :email, :password, :image)
+    end
+
+    def render_not_found_response
+        render json: { error: "Power not found" }, status: :not_found 
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def handle_errors
+        render json: { "Unpermitted Parameters": params.to_unsafe_h.except(:controller, :action, :name, :description)}, status: :unprocessable_entity 
     end
 
 end
