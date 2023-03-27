af1f2e7aa9d88e6e8812f37cd72b04e9e7570d13
max101g <maxgithinji101@gmail.com>
2023-03-27 08:43:03 +0300

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+    before_action :authorize
+
+    private
+
+    def authorize
+      return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
     has_many :userposts
     has_many :posts, through: :userposts
 
+    validates :username, uniqueness: true
     has_secure_password
 end
