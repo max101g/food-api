581ea8f5d5b7e0acf62ffad876999fb7c4f09516
max101g <maxgithinji101@gmail.com>
2023-03-27 12:01:32 +0300

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
 
-    # before_action :authorize
+    before_action :authorize
 
-    # private
+    private
 
-    # def authorize
-    #   return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
-    # end
+    def authorize
+      return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
 end
