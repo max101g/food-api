2001d2f71f59c87d10be9697c954ea7828a5a454
max101g <maxgithinji101@gmail.com>
2023-03-31 23:48:36 +0300

    commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://main--soft-bombolone-5f8259.netlify.app'
+    origins 'https://642744e0fe257522f5a45050--soft-bombolone-5f8259.netlify.app/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
