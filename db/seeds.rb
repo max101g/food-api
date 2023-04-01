d7490c1696ba1846e0c274d0fa48e55d909d089e
max101g <maxgithinji101@gmail.com>
2023-04-01 09:53:35 +0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,12 +17,27 @@ puts "seeding... 🌱"
     )
 end 
 
-20.times do
-    Post.create(
-        image:Faker::LoremFlickr.image,
-        location:Faker::Address.city
-    )
-end
+    Post.create(image:"https://bit.ly/40Pvrjt", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/42Ufp9Q", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/42ZcAUK", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3zoxJdv", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3TZUzle", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3zjTL1f", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3ZCvWMG", location:Faker::Address.city)
+    Post.create(image:"https://cnn.it/3M8d3yf", location:Faker::Address.city)
+    Post.create(image:"https://cnn.it/3M6IMja", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/42XApwj", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/40vz2Dj", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3M8xnzA", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3JVKpNY", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/42UeHcC", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3ZCwYZ4", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3M2E3PA", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3m6gJpk", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/40Tavb8", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/42UI0Mf", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/40RjaLf", location:Faker::Address.city)
+    Post.create(image:"https://bit.ly/3nBsqF7", location:Faker::Address.city)
 
 20.times do
     Userpost.create(
