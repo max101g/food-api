bba3da4de2b1f455ad0d72769cfb3979a44d0042
max101g <maxgithinji101@gmail.com>
2023-04-03 09:01:26 +0300

    commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://food-gram-d2qbmxiy9-maxwellgithinji-moringaschoo.vercel.app/'
+    origins 'https://food-gram-q87j.vercel.app/'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
