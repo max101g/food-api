2d12a804e0d70449028020c355913495487718b7
max101g <maxgithinji101@gmail.com>
2023-04-03 08:08:33 +0300

    Link vercel

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://642744e0fe257522f5a45050--soft-bombolone-5f8259.netlify.app'
+    origins 'https://food-gram-d2qbmxiy9-maxwellgithinji-moringaschoo.vercel.app/'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
