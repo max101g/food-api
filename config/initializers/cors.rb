1ff22c5c3337c0dbe047e0da8149a6c536d6b421
max101g <maxgithinji101@gmail.com>
2023-04-03 09:11:59 +0300

    commit 2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://food-gram-q87j.vercel.app/'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    origins 'https://food-gram-q87j.vercel.app'
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true, expose: ['Access-Control-Allow-Origin']
   end
 end
